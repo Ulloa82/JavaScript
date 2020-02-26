5117d228a55a9388ed5c5a67707be1d1ccf0f2c8
Ulloa82 <jose.a.ulloa.h@gmail.com>
2020-02-26 06:40:58 -0700

    First JS with console commit

---
+++ Exercise 01/index.js
@@ -0,0 +1,14 @@
+const birth='26.02.1982';
+const name='Jose';
+const lastName='Ulloa';
+
+let city='Calgary';
+let age=38;
+let nextcity= city;
+
+console.log('Birthday: ', birth);
+console.log('Name: ', name);
+console.log('LastName: ', lastName);
+console.log('City:', city);
+console.log('Age: ', age);
+console.log('NextCity: ', nextcity);
