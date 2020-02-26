ec0f3fa9c872315910e666dbc81299bda1e3ca04
Ulloa82 <jose.a.ulloa.h@gmail.com>
2020-02-26 16:38:26 -0700

    JS compare an pop up windows

---
+++ Exercise 03/index.js
@@ -0,0 +1,8 @@
+
+alert("Welcome to my site");
+
+num1=prompt('Please enter number 1: ' );
+num2=prompt('Please enter number 2: ' );
+
+let compare=confirm('Is number 1 greater than number 2?');
+alert(num1>num2);
