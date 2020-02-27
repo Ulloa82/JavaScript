58e63b3f24d9f1492dddee703279ed01cd5b0c7e
Ulloa82 <jose.a.ulloa.h@gmail.com>
2020-02-26 17:00:41 -0700

    JS conditional if / Else if / else

---
+++ Exercise 04/index.js
@@ -0,0 +1,15 @@
+alert('Welcome to a new exercise');
+
+num1=prompt('Please enter number 1: ');
+num2=prompt('Please enter number 2: ');
+
+if (num1>num2){
+    alert('Number 1 is greater than Number 2')
+}
+else if (num1<num2){
+    alert('Number 2 is greater than Number 1')
+}
+else {
+    alert('Number 1 is equal to Number 2')
+}
+
