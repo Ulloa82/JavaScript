6204adb8f0fc2d3844a1b17f2613d0ee3c64b38f
Ulloa82 <jose.a.ulloa.h@gmail.com>
2020-02-26 17:43:04 -0700

    JS Logical Operators with conditionals

---
+++ Exercise 05/index.js
@@ -0,0 +1,12 @@
+hour=prompt('Please enter the current hour format 24: ');
+if (hour<0 || hour>23){
+    alert('The format is incorrect')
+}
+else if (hour<8 || hour>=18) {
+    alert('Not office Hour')
+}
+else if (hour>=8 && hour<18){
+    alert('Office hours')
+}   
+
+
