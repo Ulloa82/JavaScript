06f26b386e212cdf0660d9c56c011b23f87b74b1
Ulloa82 <jose.a.ulloa.h@gmail.com>
2020-03-04 06:32:19 -0700

    JS loops practice

---
+++ Exercise 06/index.js
@@ -0,0 +1,25 @@
+//for loop
+for (let i = 0; i <= 1000; i++) {
+  console.log(i);
+}
+
+//While loop
+let i = 0;
+while (i <= 10) {
+  console.log(i);
+  i++;
+}
+
+//do...while loop
+let j = 100;
+do {
+  console.log(j);
+  j = j - 1;
+} while (j >= 0);
+
+//for loop even numbers
+for (let i = 0; i <= 100; i++) {
+  if (i % 2 === 0) {
+    console.log(i);
+  }
+}
