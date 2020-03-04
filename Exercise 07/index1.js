3812395cb6379378d2717945e1c374ba982c8727
Ulloa82 <jose.a.ulloa.h@gmail.com>
2020-03-04 07:17:03 -0700

    JS If statement, Switch statement

---
+++ Exercise 07/index1.js
@@ -0,0 +1,16 @@
+let day = prompt("Enter the Number");
+if (day == 1) {
+  alert("The day is Monday");
+} else if (day == 2) {
+  alert("The day is Tuesday");
+} else if (day == 3) {
+  alert("The day is Wednesday");
+} else if (day == 4) {
+  alert("The day is Thursday");
+} else if (day == 5) {
+  alert("The day is Friday");
+} else if (day == 6) {
+  alert("The day is Saturday");
+} else if (day == 7) {
+  alert("The day is Sunday");
+}
---
+++ Exercise 07/index2.js
@@ -0,0 +1,25 @@
+day = +prompt("Enter the Number");
+
+switch (day) {
+  case 1:
+    alert("The day is Monday");
+    break;
+  case 2:
+    alert("The day is Tuesday");
+    break;
+  case 3:
+    alert("The day is Wednesday");
+    break;
+  case 4:
+    alert("The day is Thursday");
+    break;
+  case 5:
+    alert("The day is Friday");
+    break;
+  case 6:
+    alert("The day is Saturday");
+    break;
+  case 7:
+    alert("The day is Sunday");
+    break;
+}
